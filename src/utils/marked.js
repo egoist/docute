0b6a76e7963498648da14156dcdfc4bbdecb7e2d
EGOIST <0x142857@gmail.com>
2016-12-31 02:23:38 +0800

    fix marked export

--- src/utils/marked.js
+++ src/utils/marked.js
@@ -4,8 +4,6 @@
  * https://github.com/chjj/marked
  */
 
-;(function() {
-
 /**
  * Block-Level Grammar
  */
@@ -1277,14 +1275,4 @@ marked.inlineLexer = InlineLexer.output;
 
 marked.parse = marked;
 
-if (typeof module !== 'undefined' && typeof exports === 'object') {
-  module.exports = marked;
-} else if (typeof define === 'function' && define.amd) {
-  define(function() { return marked; });
-} else {
-  this.marked = marked;
-}
-
-}).call(function() {
-  return this || (typeof window !== 'undefined' ? window : global);
-}());
+export default marked
