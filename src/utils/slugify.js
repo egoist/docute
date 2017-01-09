07f2f8a57a6c385645a884a6dc75b6e2842b4772
EGOIST <0x142857@gmail.com>
2017-01-09 23:48:36 +0800

    tweak regexp order

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,11 +1,13 @@
 export default function (input) {
-  input = input.replace(/[!\"#$%&'\(\)\*\+,\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '') // eslint-disable-line no-useless-escape
+  input = input
+     // remove html tags
+    .replace(/<(?:.|\n)*?>/gm, '')
+    .replace(/[!\"#$%&'\(\)\*\+,\/:;<=>\?\@\[\\\]\^`\{\|\}~]/g, '') // eslint-disable-line no-useless-escape
     // Replace dots and spaces with a sepeator
     .replace(/(\s|\.)/g, '-')
     // Convert 2 or more sepeators into just one sepeator
     .replace(/-+/g, '-')
-    // remove html tags
-    .replace(/<(?:.|\n)*?>/gm, '')
+
     // Make the whole thing lowercase
     .toLowerCase()
   // make sure the string does not start with number
