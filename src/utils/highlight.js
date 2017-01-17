613df64fc0d5a5421f91990f82ed471981a22fd2
EGOIST <0x142857@gmail.com>
2017-01-17 18:55:02 +0800

    expose highlight function

--- src/utils/highlight.js
+++ src/utils/highlight.js
@@ -1,23 +1,12 @@
-// import hljs from 'highlight.js/lib/highlight'
-
-// hljs.registerLanguage('javascript', require('highlight.js/lib/languages/javascript'))
-// hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
-// hljs.registerLanguage('vue', require('highlight.js/lib/languages/xml'))
-// hljs.registerLanguage('css', require('highlight.js/lib/languages/css'))
-// hljs.registerLanguage('cpp', require('highlight.js/lib/languages/cpp'))
-// hljs.registerLanguage('bash', require('highlight.js/lib/languages/bash'))
-// hljs.registerLanguage('markdown', require('highlight.js/lib/languages/markdown'))
-// hljs.registerLanguage('yaml', require('highlight.js/lib/languages/yaml'))
-
-// if (typeof window !== 'undefined') {
-//   window.hljs = hljs
-// }
-
 import Prism from 'prismjs'
 import 'prismjs/components/prism-yaml'
 import 'prismjs/components/prism-markdown'
 import 'prismjs/components/prism-bash'
+import store from 'store'
 
 export default function highlight(str, lang) {
+  if (typeof store.state.config.highlight === 'function') {
+    return store.state.config.highlight(str, lang)
+  }
   return Prism.highlight(str, Prism.languages[lang] || Prism.languages.markup)
 }
