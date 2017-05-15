ba3736b0e40f0d0ac72cfaf20831f4a857e97fdc
EGOIST <0x142857@gmail.com>
2017-05-15 11:43:34 +0800

    change jump container to window on mobile

--- src/utils/jump.js
+++ src/utils/jump.js
@@ -1,11 +1,14 @@
 import headTo from 'head-to'
+import { isMobile } from './dom'
 
 export default function (id, callback) {
   headTo(`#${id}`, {
     duration: 300,
     a11y: true,
     offset: -20,
+    offset: isMobile ? -70 : -20,
     callback,
-    container: '.main'
+    container: '.main',
+    container: isMobile ? window : '.main'
   })
 }
