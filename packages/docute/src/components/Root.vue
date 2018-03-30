cbec392ae160eceaa7a8068560657ea4208662ae
EGOIST <0x142857@gmail.com>
2018-03-30 21:19:00 +0800

    fix css for code blocks

--- packages/docute/src/components/Root.vue
+++ packages/docute/src/components/Root.vue
@@ -10,7 +10,7 @@ export default {
 }
 </script>
 
-<style src="prismjs/themes/prism-tomorrow.css"></style>
+<style src="../assets/css/prism-tomorrow.css"></style>
 <style src="../assets/css/nprogress.css"></style>
 <style src="../assets/css/common.css"></style>
 <style src="../assets/css/markdown.css"></style>
