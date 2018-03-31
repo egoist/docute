0989c749722de6ebd6e381b28e28ce2e923c0366
ulivz <472590061@qq.com>
2018-04-01 00:47:37 +0800

    chore(css-helper): remove comment
    refer to https://github.com/markdown-it/markdown-it-container/issues/22

--- packages/docute/src/utils/markdown-it/cssHelpers.js
+++ packages/docute/src/utils/markdown-it/cssHelpers.js
@@ -9,11 +9,8 @@ export default md => ([
     validate (params) {
       return params.trim().match(RE)
     },
-
     render(tokens, idx) {
       const [, helperType] = tokens[idx].info.trim().match(RE) || []
-      // TODO Temporarily using div instead of p is because a bug of markdown-it-container
-      // Refer to https://github.com/markdown-it/markdown-it-container/issues/22
       if (tokens[idx].nesting === 1) {
         return `<div class="${helperType}">\n` // opening tag
       }
