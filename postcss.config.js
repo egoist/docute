2b399d246386cb0ce64c288143b5d626e7ea3a35
EGOIST <0x142857@gmail.com>
2018-04-01 19:49:00 +0800

    get rid of lerna

--- postcss.config.js
+++ postcss.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   plugins: [
     require('postcss-import')({
-      path: [path.resolve('packages/docute/src/assets/css')]
+      path: [path.resolve('src/assets/css')]
     }),
     require('postcss-mixins')(),
     require('postcss-cssnext')({
--- website/poi.config.js
+++ website/poi.config.js
@@ -7,8 +7,8 @@ module.exports = {
   staticFolder: __dirname,
   extendWebpack(config) {
     config.resolve.alias
-      .set('docute$', path.join(__dirname, '../packages/docute/src/index.js'))
-      .set('@', path.join(__dirname, '../packages/docute/src'))
+      .set('docute$', path.join(__dirname, '../src/index.js'))
+      .set('@', path.join(__dirname, '../src'))
       .set('vue$', 'vue/dist/vue')
 
     config.node.set('fs', 'empty').set('net', 'empty')
