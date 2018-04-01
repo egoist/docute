b904ba5f2804e356ffdf4141fc4493b01b336dd9
EGOIST <0x142857@gmail.com>
2018-04-01 18:52:10 +0800

    simplify

--- packages/docute/src/utils/markdown-it/__test__/messageBlocks.test.js
+++ packages/docute/src/utils/markdown-it/__test__/messageBlocks.test.js
@@ -1,9 +1,10 @@
 import MarkdownIt from 'markdown-it'
 import messageBlocksPlugin from '../messageBlocks'
 
+const md = new MarkdownIt()
+md.use(messageBlocksPlugin())
+
 test('main', () => {
-  const md = new MarkdownIt()
-  md.use(messageBlocksPlugin())
   const html = md.render(`
 > __Info__: This is a \`info\`!
 
@@ -11,7 +12,17 @@ test('main', () => {
 
 > __Success__: This is __ok__!
 
-> __Note__: This is just a _note_!
+>   __Note__: This is just a _note_!
 `)
-  expect(html).toMatchSnapshot('html')
+  expect(html).toMatchSnapshot()
+})
+
+test('no matching', () => {
+  const html = md.render(`
+> __Info__ : hehe
+
+> nah
+`)
+
+  expect(html).toMatchSnapshot()
 })
