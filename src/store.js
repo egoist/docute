a9cabd82af092f45dfd5476356c823a3999ec6b9
EGOIST <0x142857@gmail.com>
2018-09-28 11:11:20 +0800

    extract marked renderer to a single file

--- src/store.js
+++ src/store.js
@@ -4,7 +4,8 @@ import loadjs from 'loadjs'
 import fetch from 'unfetch'
 import marked from './utils/marked'
 import highlight from './utils/highlight'
-import { slugify, getFilenameByPath } from './utils'
+import { getFilenameByPath } from './utils'
+import markedRenderer from './utils/markedRenderer'
 
 Vue.use(Vuex)
 
@@ -47,7 +48,7 @@ const store = new Vuex.Store({
   },
 
   actions: {
-    async fetchFile({ commit, dispatch, getters }, path) {
+    async fetchFile({ commit, getters, dispatch }, path) {
       commit('TOGGLE_SIDEBAR', false)
       commit('SET_FETCHING', true)
       const file = getFilenameByPath(getters.config.sourcePath, path)
@@ -55,94 +56,17 @@ const store = new Vuex.Store({
         fetch(file).then(res => res.text()),
         dispatch('fetchPrismLanguages')
       ])
-      const renderer = new marked.Renderer()
 
-      const headings = []
-      const slugs = []
-      renderer.heading = function(text, level, raw) {
-        let slug = slugify(raw)
-        slugs.push(slug)
-        const sameSlugCount = slugs.filter(v => v === slug).length
-        if (sameSlugCount > 1) {
-          slug += `-${sameSlugCount}`
-        }
-
-        if (level === 1) {
-          store.commit('SET_PAGE_TITLE', text)
-        } else if (level === 2 || level === 3) {
-          headings.push({
-            level,
-            raw,
-            // Remove trailing HTML
-            text: raw.replace(/<.*>\s*$/g, ''),
-            slug
-          })
-        }
-
-        const tag = `h${level}`
-        return `<${tag} id="${slug}">${text}</${tag}>`
-      }
-
-      // Disable template interpolation in code
-      renderer.codespan = text => `<code v-pre>${text}</code>`
-      const origCode = renderer.code
-      renderer.code = function(code, lang, excaped) {
-        const codeOptsRE = /({.+})/
-        let codeOpts = {}
-        if (lang && codeOptsRE.test(lang)) {
-          codeOpts = codeOptsRE.exec(lang)[1].trim()
-          try {
-            // eslint-disable-next-line no-new-func
-            const fn = new Function(`return ${codeOpts}`)
-            codeOpts = fn()
-          } catch (err) {
-            throw new Error(
-              `You're using invalid options for code fences, it must be JSON or JS object!\n${
-                err.message
-              }`
-            )
-          }
-          lang = lang.replace(codeOptsRE, '').trim()
-        }
-
-        let res = origCode
-          .call(this, code, lang, excaped)
-          .replace(/^<pre>/, '<pre v-pre>')
-
-        if (codeOpts.highlight) {
-          const codeMask = code
-            .split('\n')
-            .map((v, i) => {
-              i += 1
-              const shouldHighlight = codeOpts.highlight.some(number => {
-                if (typeof number === 'number') {
-                  return number === i
-                }
-                if (typeof number === 'string') {
-                  const [start, end] = number.split('-').map(Number)
-                  return i >= start && (!end || i <= end)
-                }
-                return false
-              })
-              return shouldHighlight
-                ? `<span class="code-line highlighted">&#8203;</span>`
-                : `<span class="code-line">&#8203;</span>`
-            })
-            .join('')
-          res += `<div class="code-mask">${codeMask}</div>`
-        }
-
-        return `<div data-lang="${lang || ''}" class="pre-wrapper">${res}</div>`
-      }
-
+      const env = {}
       commit(
         'SET_HTML',
         marked(text, {
-          renderer,
+          renderer: markedRenderer(env),
           highlight
         })
       )
-      commit('SET_PAGE_HEADINGS', headings)
+      commit('SET_PAGE_TITLE', env.title)
+      commit('SET_PAGE_HEADINGS', env.headings)
       commit('SET_FETCHING', false)
     },
 
