97e34e055f331254353b707c1c674b4510287e61
EGOIST <0x142857@gmail.com>
2018-12-15 01:13:41 +0800

    fix: config for default export
    fix #192

--- build/poi.lib.config.js
+++ build/poi.lib.config.js
@@ -2,18 +2,6 @@ const pkg = require('../package')
 
 module.exports = {
   entry: 'src/index.js',
-  plugins: [
-    {
-      resolve: {
-        apply(api) {
-          api.hook('onCreateWebpackConfig', config => {
-            config.resolve.alias.set('vue$', 'vue/dist/vue.esm.js')
-            config.output.libraryExport('default')
-          })
-        }
-      }
-    }
-  ],
   output: {
     format: 'umd',
     moduleName: 'Docute',
@@ -26,5 +14,9 @@ module.exports = {
   constants: {
     __DOCUTE_VERSION__: JSON.stringify(pkg.version),
     __PRISM_VERSION__: JSON.stringify(require('prismjs/package').version)
+  },
+  chainWebpack(config) {
+    config.resolve.alias.set('vue$', 'vue/dist/vue.esm.js')
+    config.output.libraryExport('default')
   }
 }
