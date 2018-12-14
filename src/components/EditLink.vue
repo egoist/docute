67877a0162a3f254bbf08fc3799f1512ed201533
EGOIST <0x142857@gmail.com>
2018-12-14 23:48:40 +0800

    fix: hide edit link when editLinkBase is not provided
    fix #191

--- src/components/EditLink.vue
+++ src/components/EditLink.vue
@@ -1,9 +1,24 @@
 <template>
   <div v-if="editLink" class="EditLink">
     <a target="_blank" :href="`${editLink}`">
-      <svg class="icon" id="i-compose" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="currentcolor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-        <path d="M27 15 L27 30 2 30 2 5 17 5 M30 6 L26 2 9 19 7 25 13 23 Z M22 6 L26 10 Z M9 19 L13 23 Z" />
-      </svg> {{ editLinkText }}</a>
+      <svg
+        class="icon"
+        id="i-compose"
+        viewBox="0 0 32 32"
+        width="32"
+        height="32"
+        fill="none"
+        stroke="currentcolor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+      >
+        <path
+          d="M27 15 L27 30 2 30 2 5 17 5 M30 6 L26 2 9 19 7 25 13 23 Z M22 6 L26 10 Z M9 19 L13 23 Z"
+        ></path>
+      </svg>
+      {{ editLinkText }}
+    </a>
   </div>
 </template>
 
@@ -20,8 +35,13 @@ export default {
         return editLink
       }
       if (file) {
-        return /^https?:\/\//.test(file) ? file : this.editLinkBase + file
+        return /^https?:\/\//.test(file)
+          ? file
+          : this.editLinkBase
+          ? this.editLinkBase + file
+          : null
       }
+      return null
     },
 
     editLinkText() {
