81f0a7bae9da4c67eafc1c5dbb9b308692888ec3
EGOIST <0x142857@gmail.com>
2018-12-16 15:01:10 +0800

    chore: let semantic-release generate a CHANGELOG.md

---
+++ release.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  branch: 'master',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md'
+      }
+    ],
+    '@semantic-release/npm',
+    '@semantic-release/git',
+    '@semantic-release/github'
+  ]
+}
