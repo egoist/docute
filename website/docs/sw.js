7db5b1e26316a33170eead72db53c627eac45c01
EGOIST <0x142857@gmail.com>
2019-03-23 01:41:42 +0800

    chore: remove service worker

--- website/docs/sw.js
+++ website/docs/sw.js
@@ -1,20 +1,14 @@
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js'
-)
-
 self.addEventListener('install', e => {
   self.skipWaiting()
 })
 
-const ALLOWED_HOSTS = [
-  // The domain to load markdown files
-  location.host,
-  // The domain to load external scripts like Prism components
-  'unpkg.com'
-]
-
-const matchCb = ({ url, event }) => {
-  return event.request.method === 'GET' && ALLOWED_HOSTS.includes(url.host)
-}
-
-workbox.routing.registerRoute(matchCb, workbox.strategies.networkFirst())
+self.addEventListener('activate', e => {
+  self.registration
+    .unregister()
+    .then(() => {
+      return self.clients.matchAll()
+    })
+    .then(clients => {
+      clients.forEach(client => client.navigate(client.url))
+    })
+})
