5095a2f5b3d2d8f437a38f8efa371415b1ed6f76
evillt <ijoec123@gmail.com>
2019-07-04 13:15:46 +0800

    feat(marked): support `@` in attribute names

--- src/utils/marked.js
+++ src/utils/marked.js
@@ -583,7 +583,7 @@ inline.autolink = edit(inline.autolink)
   .replace('email', inline._email)
   .getRegex()
 
-inline._attribute = /\s+[a-zA-Z:_][\w.:-]*(?:\s*=\s*"[^"]*"|\s*=\s*'[^']*'|\s*=\s*[^\s"'=<>`]+)?/
+inline._attribute = /\s+[a-zA-Z@:_][\w.:-]*(?:\s*=\s*"[^"]*"|\s*=\s*'[^']*'|\s*=\s*[^\s"'=<>`]+)?/
 
 inline.tag = edit(inline.tag)
   .replace('comment', block._comment)
