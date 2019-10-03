210f6a6dcbd4d81825a3385a2a906fd05d256003
EGOIST <0x142857@gmail.com>
2019-10-03 18:55:22 +0800

    fix: tweak style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -187,7 +187,6 @@ export default {
   padding-top: calc(var(--header-height) + 40px);
   padding-bottom: 2rem;
   background: var(--main-background);
-  min-height: 100vh;
 
   @media screen and (max-width: 768px) {
     padding-left: 0;
